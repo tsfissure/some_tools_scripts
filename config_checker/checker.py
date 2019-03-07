b7a1716a1c96feba758e7d021983c3c197b77d85
tsfissure <tsfissure@gmail.com>
2019-03-07 14:35:08 +0800

    update config_checker

--- config_checker/checker.py
+++ config_checker/checker.py
@@ -10,6 +10,8 @@ import sys
 import xlrd
 import traceback
 import ctypes
+import csv
+import codecs
 
 
 STD_OUTPUT_HANDLE= -11
@@ -27,8 +29,12 @@ class ConfigParam(object):
     def __init__(self):
         self.lua_path = ""
         self.xlsx_path = ""
+        self.csv_path = ""
         self.isdebug = False
         self.ignore = []
+        self.luaFiles = []
+        self.csvFiles = []
+        self.xlsxFiles = []
 
     def load(self):
         with open("checker.cfg", "r") as f:
@@ -39,14 +45,27 @@ class ConfigParam(object):
                 elif "xlsx_path" in line:
                     a, b = line[:-1].split('=')
                     self.xlsx_path = b.strip()
+                elif "csv_path" in line:
+                    a, b = line[:-1].split('=')
+                    self.csv_path = b.strip()
                 elif "ignore" in line:
                     a, b = line[:-1].split('=')
                     self.ignore = [i.strip() for i in b.split(',')]
-        print u'lua_path: [%s]\nxlsx_path: [%s]\nignorefiles: %s\n\n' % (self.lua_path, self.xlsx_path, self.ignore)
+            print u'lua_path: [%s]\ncsv_path: [%s]\nxlsx_path: [%s]\nignorefiles: %s\n\n' % (self.lua_path, self.csv_path, self.xlsx_path, self.ignore)
 
     def isIgnore(self, fd):
         return fd in self.ignore
 
+    def loadFilesInternal(self, path, suffix):
+        rlt = []
+        for root, dirs, files in os.walk(path):
+            rlt += [fd for fd in files if fd.endswith(suffix) and not fd.startswith("~") and not self.isIgnore(fd)]
+        return rlt
+
+    def loadFiles(self):
+        self.luaFiles = self.loadFilesInternal(self.lua_path, ".lua")
+        self.csvFiles = self.loadFilesInternal(self.csv_path, ".csv")
+        self.xlsxFiles = self.loadFilesInternal(self.xlsx_path, ".xlsx")
 
 instCfg = ConfigParam()
 
@@ -95,7 +114,12 @@ class LuaTable(object):
                 if -1 == pos: continue
                 posL = line.find('[')
                 posR = line.find(']')
-                key = int(line[posL + 1:posR])
+                try:
+                    key = int(line[posL + 1:posR])
+                except:
+                    print u'[%s] 解析失败,格式不标准!' % fd
+                    rlt = False
+                    break
                 if self.table.get(key, None):
                     print u'[%s] key[%s] 重复了!!!' % (fd, key)
                     rlt = False
@@ -116,11 +140,42 @@ class LuaTable(object):
     def rownum(self):
         return len(self.table)
 
-
 instLuaTable = LuaTable()
 
+
+class CsvTable(object):
+
+    def __init__(self):
+        self.data = []
+
+    def loadFile(self, fd):
+        self.data = []
+        with codecs.open(os.path.join(instCfg.csv_path, fd), "r") as f:
+            try:
+                reader = csv.reader(f)
+                self.data = [row for row in reader]
+                # print self.data
+            except:
+                print u'解析[%s] 出错!' % fd
+                print traceback.print_exc(5)
+                return False
+        return True
+
+    def rownum(self):
+        return len(self.data)
+
+    def colnum(self):
+        if 0 == len(self.data):
+            return 0
+        return len(self.data[0])
+
+    def get(self, i, j):
+        return self.data[i][j]
+
+instCsvTable = CsvTable()
+
 class XlsxTable(object):
-    
+
     def __init__(self):
         self.data = None
         self.fdName = ""
@@ -130,19 +185,18 @@ class XlsxTable(object):
         self.data = xlrd.open_workbook(pwd)
         self.fdName = fd
 
-    def compareInternal(self, sheet, fd):
-        if instCfg.isIgnore(fd): return True
+    def compareLua(self, sheet, fd):
         types = sheet.row_values(1)
         for keyName in sheet.row_values(2):
             if len(keyName) > 0 and ord(keyName[-1]) == 10:
                 print u'[%s::%s] 字段[%s]结尾有回车,跳过检查' % (self.fdName, sheet.name, keyName[:-1])
-                return False
-        keys = [i.strip() for i in sheet.row_values(2)]
+                return
         if not instLuaTable.loadFile(fd):
             return
         if instLuaTable.rownum() + 3 != sheet.nrows:
             print u'[%s::%s] 数据行数不致! xlsx[%s] lua[%s]' % (self.fdName, sheet.name, sheet.nrows - 3,instLuaTable.rownum())
             return
+        keys = [i.strip() for i in sheet.row_values(2)]
         for i in xrange(3, sheet.nrows):
             try:
                 vkey = int(sheet.cell(i, 0).value)
@@ -168,45 +222,54 @@ class XlsxTable(object):
                     return False
         return True
 
+    def compareCsv(self, sheet, fd):
+        if not instCsvTable.loadFile(fd):
+            return False
+        if instCsvTable.rownum() < sheet.nrows:
+            print u'[%s::%s]与csv行不一样多!xlsx[%s] csv[%s]' % (self.fdName, sheet.name, sheet.nrows, instCsvTable.rownum())
+            return False
+        keys = [i.strip() for i in sheet.row_values(2)]
+        for i in xrange(sheet.nrows):
+            try:
+                vkey = int(sheet.cell(i, 0).value)
+            except:
+                vkey = i
+            for j in xrange(sheet.ncols):
+                val =  sheet.cell(i, j).value 
+                if unicode == type(val): continue
+                csvVal = instCsvTable.get(i, j)
+                check = True
+                try:
+                    check = int(val) == int(csvVal)
+                except:
+                    check = str(val) == str(csvVal)
+                if not check:
+                    print u'[%s::%s]与csv key[%s] 列[%s] 值不一样! xlsx[%s] csv[%s]' % (self.fdName, sheet.name, vkey, keys[j], val, csvVal)
+                    return False
+        return True
 
-    def compare(self, luaFiles):
-        if instCfg.isIgnore(self.fdName): return
+    def compare(self):
         for sheet in self.data.sheets():
             fd = sheet.name + '.lua'
-            if fd in luaFiles:
-                rlt = self.compareInternal(sheet, fd)
-                # if not rlt:
-                #     print u'[%s::%s] check FAIL!!!' % (self.fdName, sheet.name)
+            if fd in instCfg.luaFiles:
+                if not self.compareLua(sheet, fd):
+                    continue
+            fd = sheet.name + '.csv'
+            if fd in instCfg.csvFiles:
+                rlt = self.compareCsv(sheet, fd)
 
 
 instXlsxTable = XlsxTable()
 
-def lstLuaFiles():
-    rlt = []
-    for root, dirs, files in os.walk(instCfg.lua_path):
-        for fd in files:
-            if fd.endswith(".lua"):
-                rlt.append(fd)
-    return rlt
-
-def lstXlsxFiles():
-    rlt = []
-    for root, dirs, files in os.walk(instCfg.xlsx_path):
-        for fd in files:
-            if fd.endswith(".xlsx") and not fd.startswith("~"):
-                rlt.append(fd)
-    return rlt
-
 def gao():
-    luaFiles = lstLuaFiles()
-    xlsxFiles = lstXlsxFiles()
-    for fd in xlsxFiles:
+    for fd in instCfg.xlsxFiles:
+        # if fd != "demonstepTable.xlsx": continue
         try:
+            set_color(FOREGROUND_DARKWHITE)
             print u'compare [%s]' % fd
             set_color(FOREGROUND_RED)
             instXlsxTable.loadFile(fd)
-            instXlsxTable.compare(luaFiles)
-            set_color(FOREGROUND_DARKWHITE)
+            instXlsxTable.compare()
         except:
             print u'[%s] ERROR!!!' % fd
             print traceback.print_exc(5)
@@ -214,9 +277,11 @@ def gao():
 if __name__ == '__main__':
     try:
         instCfg.load()
+        instCfg.loadFiles()
         gao()
     except :
         print traceback.print_exc(5)
 
+    set_color(FOREGROUND_DARKWHITE)
     raw_input('Done!')
 
